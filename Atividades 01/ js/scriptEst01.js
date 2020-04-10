eff1c818ea6ec9617751c5a99f5f8a9b170570f3
Gilson Caetano <53578134+gilsoncaetano@users.noreply.github.com>
2020-04-09 23:36:37 -0300

    Calcular valor do estacionamento.

---
+++ Atividades 01/ js/scriptEst01.js
@@ -0,0 +1,20 @@
+function carro() {
+  var n1 = prompt("Quantas horas você ficou estacionado com seu carro?", "");
+  var n2 = 2;
+  valor = n1 * n2;
+  alert("O valor a ser pago é: R $" + valor + ", 00");
+}
+
+function onibus() {
+  var n1 = prompt("Quantas horas você ficou estacionado com seu Ónibus?", "");
+  var n2 = 3;
+  valor = n1 * n2;
+  alert("O valor a ser pago é: R $" + valor + ", 00");
+}
+
+function caminhao() {
+  var n1 = prompt("Quantas horas você ficou estacionado com seu caminhão?", "");
+  var n2 = 4;
+  valor = n1 * n2;
+  alert("O valor a ser pago é: R $" + valor + ", 00");
+}
